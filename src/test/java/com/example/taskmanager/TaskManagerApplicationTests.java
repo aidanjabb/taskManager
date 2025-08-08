0a663da90d5ea3daf6791f2b0aa23031c88302d7
Aidan Jabbarzadeh <aidanjabbarzadeh@mac.lan>
2025-08-08 13:09:33 -0500

    update application-test.properties, remove @TestPropertySource from test class

--- src/test/java/com/example/taskmanager/TaskManagerApplicationTests.java
+++ src/test/java/com/example/taskmanager/TaskManagerApplicationTests.java
@@ -5,20 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 
 import com.example.taskmanager.repository.UserRepository;
 
-// TODO add some comment abt what we are actually doing here, related to what the @SpringBootTest annotation actually does
-
 @SpringBootTest
+// Prevents Spring Boot from auto-switching to H2
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@TestPropertySource(properties = {
-  "spring.datasource.url=jdbc:postgresql://db:5432/taskdb",
-  "spring.datasource.username=postgres",
-  "spring.datasource.password=postgres",
-  "spring.jpa.hibernate.ddl-auto=update"
-})
 @ActiveProfiles("test")
 class TaskManagerApplicationTests {
 
