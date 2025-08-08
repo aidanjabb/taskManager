49a3d89c6175c6f2d55273b5ef7ffcbcf159a50b
Aidan Jabbarzadeh <aidanjabbarzadeh@mac.lan>
2025-08-08 11:32:20 -0500

    Explicitly exclude H2 and configure Postgres in tests

--- src/test/java/com/example/taskmanager/TaskManagerApplicationTests.java
+++ src/test/java/com/example/taskmanager/TaskManagerApplicationTests.java
@@ -4,10 +4,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 import com.example.taskmanager.repository.UserRepository;
 
 // TODO add some comment abt what we are actually doing here, related to what the @SpringBootTest annotation actually does
+
+@TestPropertySource(properties = {
+  "spring.datasource.url=jdbc:postgresql://db:5432/taskdb",
+  "spring.datasource.username=postgres",
+  "spring.datasource.password=postgres"
+})
 @ActiveProfiles("test")
 @SpringBootTest
 class TaskManagerApplicationTests {
