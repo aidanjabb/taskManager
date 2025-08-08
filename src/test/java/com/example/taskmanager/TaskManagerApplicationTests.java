578a51b9112d14bf47507bba93e84a51f9511f5f
Aidan Jabbarzadeh <aidanjabbarzadeh@mac.lan>
2025-08-08 12:35:56 -0500

    Prevent Spring Boot from auto-switching to H2

--- src/test/java/com/example/taskmanager/TaskManagerApplicationTests.java
+++ src/test/java/com/example/taskmanager/TaskManagerApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.taskmanager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
@@ -15,6 +16,7 @@ import com.example.taskmanager.repository.UserRepository;
   "spring.datasource.username=postgres",
   "spring.datasource.password=postgres"
 })
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
 @SpringBootTest
 class TaskManagerApplicationTests {
